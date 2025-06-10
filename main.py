e1703d864c466692d624d1a5bcc2674e51fcc6a9
jeroenvdmeer <1658477+jeroenvdmeer@users.noreply.github.com>
2025-06-10 17:28:22 +0200

    Cleaned up the tool, added a resource to request the DB schema, and included three example prompts.

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from mcp.server.fastmcp import FastMCP
+import uvicorn
 import logging
 import config
 import query_processor
-import uvicorn
+import database
 
 # Configure logging using level from config
 logging.basicConfig(level=config.LOG_LEVEL)
@@ -12,19 +13,46 @@ logger = logging.getLogger(__name__)
 mcp = FastMCP("Feyod MCP Server", stateless_http=True)
 
 
-@mcp.tool(
-    name="query_feyod_database",
-    description="Converts a natural language query about Feyenoord matches into a SQL query, executes it, and returns the SQL query and its result.",
-)
-async def query_feyod_database(natural_language_query: str) -> dict:
-    """MCP tool: Query Feyenoord database using natural language."""
-    logger.info(f"Received MCP tool call: {natural_language_query}")
+@mcp.resource("file:///feyod.db")
+async def get_schema() -> str:
+    """Returns the schema of the Feyod (Feyenoord Open Data)
+    database, including example rows from each table. Use this
+    information to ask better questions that are easier to
+    be answered by the tool."""
+
+    logger.info(f"Received schema request")
+    return await database.get_schema_description()
+
+
+@mcp.prompt()
+def wins_against_opponent(opponent: str) -> str:
+    """Returns a question about the number of wins Feyenoord has against a given opponent."""
+    return f"Hoe vaak heeft Feyenoord gewonnen van {opponent}?"
+
+
+@mcp.prompt()
+def two_players_scored_in_match(player1: str, player2: str) -> str:
+    """Returns a question about the number of matches in which two players both scored."""
+    return f"Hoe vaak heeft hebben {player1} en {player2} tegelijk in een wedstrijd gescoord?"
+
+
+@mcp.prompt()
+def biggest_win(opponent: str) -> str:
+    """Returns a question about the biggest win Feyenoord has against a given opponent."""
+    return f"Wat is de grootste overwinning van Feyenoord op {opponent}?"
+
+
+@mcp.tool()
+async def answer_feyenoord_question(natural_language_query: str) -> dict:
+    """Answers questions about Feyenoord. Questions can be asked in natural language,
+    text, and can be about matches (lineups, results, goals, cards, etc.), players,
+    and opponents."""
+
+    logger.info(f"Received question: {natural_language_query}")
     result = await query_processor.process_query_workflow(natural_language_query)
-    if isinstance(result, dict) and "sql_query" in result and "query_result" in result:
+
+    if isinstance(result, list):
         return result
-    elif isinstance(result, list):
-        # If only query_result is returned, return with empty sql_query
-        return {"sql_query": "", "query_result": result}
     else:
         raise Exception("Tool execution failed or returned unexpected data.")
 
